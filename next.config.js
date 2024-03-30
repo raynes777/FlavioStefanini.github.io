7eb55d3b62736994183ebadad1bf93be627b0472
Flavio Stefanini <flavio.st93@gmail.com>
2024-03-30 03:01:58 +0100

    Fix attempt

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing Next.js config options here
+};
+
+module.exports = {
+  ...nextConfig, // Spread the existing nextConfig settings
+
+  // Custom settings for GitHub Pages deployment
+  basePath: isProd ? '/FlavioStefanini.github.io' : '', // Adjust the basePath to match your repository name
+  assetPrefix: isProd ? '/FlavioStefanini.github.io/' : '', // Ensure the assetPrefix ends with a slash
+};
