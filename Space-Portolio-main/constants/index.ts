43826c4e85d8d11a85dfde157c12ec1f5f342839
Flavio Stefanini <flavio.st93@gmail.com>
2024-03-30 02:35:42 +0100

    Initial commit

---
+++ Space-Portolio-main/constants/index.ts
@@ -0,0 +1,104 @@
+export const Skill_data = [
+    {
+      skill_name: "Python",
+      Image: "/python.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Selenium",
+      Image: "/selenium.png",
+      width: 80,
+      height: 80,
+    },    
+    {
+      skill_name: "Pandas",
+      Image: "/pandas.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Numpy",
+      Image: "/numpy.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Pyqt",
+      Image: "/pyqt.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Tkinter",
+      Image: "/tk.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/instagram.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/discord.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+   
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Windows",
+      Image: "/windows.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Linux",
+      Image: "/linux.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Mac Os",
+      Image: "/macos.png",
+      width: 70,
+      height: 70,
+    },
+
+  ];
+  
+  export const Full_stack = [
+
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+
+  ];
+  
+  
+  
+  
+  
